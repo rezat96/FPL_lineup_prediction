71d2b9f5d7551767e61de8685237b526301e49ef
rezat96 <rezatalakoob@yahoo.com>
2022-10-30 15:02:04 -0400

    Add files via upload
    added python scripts to make database and do the query in the main.py

---
+++ make_db.py
@@ -0,0 +1,44 @@
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+r = requests.get(url)
+json = r.json()
+elements_df = pd.DataFrame(json['elements'])
+elements_types_df = pd.DataFrame(json['element_types'])
+teams_df = pd.DataFrame(json['teams'])
+event_df = pd.DataFrame(json['events'])
+phases_df = pd.DataFrame(json['phases'])
+#total_players_df = pd.DataFrame(json['total_players'])
+element_stats_df = pd.DataFrame(json['element_stats'])
+
+#lets pick useful columns!
+slim_elements_types_df = elements_types_df[['id', 'plural_name', 'singular_name_short','squad_select', 'squad_min_play', 'squad_max_play', 'element_count']]
+slim_elements_df = elements_df[['id','second_name','team','element_type',
+                                'selected_by_percent','chance_of_playing_next_round','value_season', 'now_cost','minutes','total_points']]
+
+slim_teams_df = teams_df[['id','name','short_name','loss','draw','win','played','points','position']]
+
+
+#Create connection to sql
+conn = sqlite3.connect('fpl.db')
+cursor = conn.cursor()
+
+command1 = """CREATE TABLE IF NOT EXISTS
+positions(id INTEGER PRIMARY KEY, plural_name TEXT, singular_name_short TEXT, squad_select INTEGER, squad_min_play INTEGER, squad_max_play INTEGER, element_count INTEGER)"""
+cursor.execute(command1)
+
+command2 = """CREATE TABLE IF NOT EXISTS
+teams(id INTEGER PRIMARY KEY, name TEXT, short_name TEXT, loss INTEGER, draw INTEGER, win INTEGER, played INTEGER, points INTEGER, position INTEGER)"""
+cursor.execute(command2)
+
+command3 = """CREATE TABLE IF NOT EXISTS
+players(id INTEGER PRIMARY KEY, second_name TEXT, team TEXT, element_type INTEGER, selected_by_percent INTEGER,
+chance_of_playing_next_round INETGER, value_season INTEGER, now_cost INTEGER, minutes INTEGER, total_points INTEGER, FOREIGN KEY(element_type) REFERENCES positions(id))"""
+cursor.execute(command3)
+
+slim_elements_df.to_sql(name= 'players', con= conn, if_exists= 'replace', index=False)
+slim_teams_df.to_sql(name= 'teams', con= conn, if_exists= 'replace', index=False)
+slim_elements_types_df.to_sql(name= 'positions', con= conn, if_exists= 'replace', index=False)
+conn.commit()
